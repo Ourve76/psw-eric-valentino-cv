c9f0e752b2b982cac14b2cb0a26a57dcebc30329
Ourve76 <Ericvalentino76@gmail.com>
2023-03-21 11:44:11 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Eric Valentino | CV</title>
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm ">
+  <div class="container">
+    <a class="navbar-brand" href="#">Eric Valentino</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Projects</a>
+        <a class="nav-link" href="#">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    
+<div class="jumbotron text-center mb-5">
+    <img src="img/profile.jpg" alt="profile picture" width="200" class="rounded-circle img-thumbnail"/>
+  <h1 class="display-4">Eric Valentino</h1>
+  <p class="lead">Student | Information System</p>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 info-panel",>
+            <div class="row">
+                <div class="col-lg">
+                    <img src="img/adobe.jpg" class="rounded" alt="Adobe" style="float: left;">
+                    <h4>Video Editor</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/figma.png" class="rounded" alt="Figma" style="float: left;">
+                    <h4>UI/UX</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/steam.png" class="rounded" alt="Steam" style="float: left;">
+                    <h4>Gamers</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section id="about">
+    <div class="container">
+        <div class="text-center m-5">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vel praesentium debitis non iusto quo esse! Maiores explicabo asperiores eum quibusdam tempore id pariatur, tempora quam animi dolores labore assumenda mollitia facere officiis. Fuga officia excepturi expedita eligendi! Nihil delectus eum vero optio. Sit, nam nobis quia, non adipisci corrupti laboriosam perspiciatis minus ratione hic libero, molestias aliquam beatae! Error pariatur, corrupti vitae, nihil incidunt magnam numquam libero eos at perferendis eveniet! Doloremque, optio commodi.</p>
+        </div>
+    </div>
+</section>
+
+<section class="projects">
+    <div class="container" style="justify-content: space-between; display:flex;">
+        <div>
+            <h2>My project</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, labore. Possimus hic nostrum quasi sequi.</p>
+        </div>
+        <div>
+            <a href="https://github.com/Ourve76" class="btn btn-outline-secondary">View More</a>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/project.jpg" class="card-img-top" alt="Project1">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/project.jpg" class="card-img-top" alt="Project2">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mt">
+                <img src="img/project.jpg" class="card-img-top" alt="Project4">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 3</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>   
+            <div class="col-4">
+                <div class="card mt-3">
+                <img src="img/project.jpg" class="card-img-top" alt="Project5">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 4</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mt-3">
+                <img src="img/project.jpg" class="card-img-top" alt="Project5">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 5</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mt-3">
+                    <img src="img/project.jpg" class="card-img-top" alt="Project5">
+                    <div class="card-body">
+                        <h5 class="card-title">Project 6</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="contacts">
+    <div class="container bg-danger">
+        <div style="justify-content: space-between; display:flex;">
+        <div class="row text-white">
+                <div class="col">
+                    <h2>Contacts</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus officia distinctio accusantium quia unde tempore deserunt, voluptatibus, error magnam quos cum vero commodi similique reprehenderit earum dolores eos? Nobis, quam.</p>
+                </div>
+            <div class="col">
+                <img src="img/footer.jpg" alt="contact" class="img-fluid rounded-3">
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<div class="footer">
+<footer class="text-white text-center">
+    <p>Created by <a href="https://www.instagram.com/eric_valentino76" class="text-white fw-bold">Eric Valentino</a></p>
+</footer>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
